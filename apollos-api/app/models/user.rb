b8a3fe5fb0669cae5f5f664cbdb6aed3689b6e49
David Whitlatch <davidtwhitlatch@gmail.com>
2020-06-01 15:07:40 -0400

    updated routes

--- apollos-api/app/models/user.rb
+++ apollos-api/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
--- apollos-api/config/routes.rb
+++ apollos-api/config/routes.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  resources :users do
+    resources :posts do
+      resources :comments
+    end
+  end
+  resources :posts
+  resources :categories
+  # get '/posts/:post_id/comments', to: 'comments#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
