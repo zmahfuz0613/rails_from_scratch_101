fb3d5b6ca365a44f9ab8b67b38833b5a1094b7bf
David Whitlatch <davidtwhitlatch@gmail.com>
2020-06-01 17:04:17 -0400

    goofed around with controllers and routes

--- apollos-api/app/controllers/users_controller.rb
+++ apollos-api/app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
-class UsersController < ApplicationController
+# frozen_string_literal: true
 
-  
+class UsersController < ApplicationController
+  def show
+    @database_users = [{ name: 'Soleil' }, { name: 'Joe' }, { name: 'David' }]
+    @user = @database_users.find { |user| user[:name] == params[:name] }
+
+    render json: @user
+  end
+
+  def update
+    puts "the id in the path is #{params[:id]}"
+    puts "the username in the request body is #{params[:username]}"
+  end
 end
--- apollos-api/config/routes.rb
+++ apollos-api/config/routes.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users do
-    resources :posts do
-      resources :comments
-    end
-  end
-  resources :posts
-  resources :categories
+  get '/users/:name', to: 'users#show'
+  # resources :users do
+  #   resources :posts do
+  #     resources :comments
+  #   end
+  # end
+  # resources :posts
+  # resources :categories
   # get '/posts/:post_id/comments', to: 'comments#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
